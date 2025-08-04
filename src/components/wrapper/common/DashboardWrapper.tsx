2e876dcadf49ba49306c4e5807044072856a407f
Nikhil Kumar <lnbteamtesting@gmail.com>
2025-08-04 16:36:31 +0530

    dev

--- src/components/wrapper/common/DashboardWrapper.tsx
+++ src/components/wrapper/common/DashboardWrapper.tsx
@@ -9,7 +9,7 @@ interface Type{
 
 export default function DashboardWrapper({changeBg=false,heading,className,children}:Type){
 return(
-       <div className={`px-8 py-15 h-screen container mx-auto ${changeBg ? "bg-peach2" : "bg-black"} ${className}`}>
+       <div className={`px-8 py-15 md:h-screen container mx-auto ${changeBg ? "bg-peach2" : "bg-black"} ${className}`}>
             <h3 className={`uppercase text-2xl md:text-3xl xl:text-[3rem] mb-2 xl:mb-8 ${changeBg ? "text-black" : "text-peach2"} `}>{heading}</h3>
             {children}
         </div>
